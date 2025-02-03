0526252aea64fed0b484577a83b52c3077962aa5
VaishnaviAitha <aithavaishnavi1517@gmail.com>
2025-02-03 19:23:49 +0530

    Version

--- app/src/main/java/com/siddartharao/candys/StartActivity.java
+++ app/src/main/java/com/siddartharao/candys/StartActivity.java
@@ -3,6 +3,7 @@ package com.siddartharao.candys;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -36,6 +37,6 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
-
+        Log.e("Name","Vaish");
     }
 }
